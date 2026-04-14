797925031ddd77184cae6b5eb1cef51ef398e080
Rodrigo <rodritomeflo@gmail.com>
2026-04-14 11:45:36 -0400

    primer commit

---
+++ prueba.py
@@ -0,0 +1,18 @@
+# # print("ingrese nombre")
+# # nombre=input()
+# # edad=int(print("ingrese su edad: ", ))
+
+# # print("hola", nombre, "su edad en 5 años será", edad+5)
+
+# numeros_del_usuario=int(print("Ingrese un número: "))
+# resultados_suma=numeros_del_usuario+numeros_del_usuario
+# resultados_resta=numeros_del_usuario-numeros_del_usuario
+# print("Su número ingresado sumado por si mismo da", resultados_suma) 
+# print("y su i resultado restado por si mismo da", resultados_resta)
+
+
+
+
+
+
+print("gg")
