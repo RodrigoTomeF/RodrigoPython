bdd075c37faf324a07ebdfd21844e330e9575845
RodrigoTomeF <rodritomeflo@gmail.com>
2026-04-28 13:24:57 -0400

    ejerciciospy

---
+++ randomly.py
@@ -0,0 +1,89 @@
+# #Uso y ejemplo de random
+
+import random
+# # dado=random.randint(1,10)  #Int por que es un número entero.
+# # print(dado)
+
+# # for i in range(dado):
+# #     print("Hola deny")
+
+# dado1=random.randint(1,10)
+
+# dado2=random.randint(1,10)
+
+
+# print(f"El dado 1 dio {dado1}") 
+# print(f"El dado 2 dio {dado2}")
+
+# if dado1==dado2:
+#     print("Se va a la carsel")
+# else:
+#     print("Avance por favor")
+
+
+# import random
+# Contador=0
+# resultado_del_número_random=random.randint(1,100)
+# Adivinansa_del_usuario=print(input("Ingrese un número para adivinar entre 1 y 100 "))
+
+# while Contador >= 5:
+
+#     if Adivinansa_del_usuario > resultado_del_número_random:
+#         print("Te pasaste")
+#     else:
+#         print("El número a adivinar es mayor")
+
+# import random
+
+# print("Intenta adivinar un número")
+
+# num=random.randint(1,100)
+# intentos=5
+# while intentos > 0:
+#     adivina=int(input("Ingresa un número del 1 al 100:"))
+#     print("")
+# import time
+# hp1=100
+# hp2=100
+# player1=input("Ingresa al primer luchador:")
+# player2=input("Ingresa al segundo luchador:")
+
+
+# while hp1 > 2 and hp2 > 2:
+#     turno=random.randint(1,2)
+#     if turno %2==0:
+#         golpe=random.randint(7,18)
+#         hp2=hp2-golpe
+#         print(f"El luchador {player1} golpea al luchador {player2}")
+#         print(f"El luchador {player2} le quedan {hp2} de vida")
+#     else:
+#         golpe=random.randint(7,18)
+#         hp1=hp1-golpe
+#         print(f"El luchador {player2} golpea al luchador {player1}")
+#         print(f"El luchador {player1} le quedan {hp1} de vida")
+#     time.sleep(2)
+# if hp1 <= 2:
+#     print(f"El jugador {player1} a perdido")
+    
+
+
+
+
+
+
+# j1=random.randint(60,190)
+# j2=random.randint(60,190)
+# j3=random.randint(60,190)
+# print(f"El jugador 1 lanzo la pelota {j1} metros")
+# print(f"El jugador 2 lanzo la pelota {j2} metros")
+# print(f"El jugador 3 lanzo la pelota {j3} metros")
+
+# if j1>j2 and j1>j3:
+#     print("El jugador 1, lanzo la pelota más lejos")
+# elif j2>j3:
+#     print("El jugador 2, lanzo la pelota más lejos")
+# else:
+#     print("El jugador 3, lanzo la pelota más lejos")
+
+
+#ponerle un imite al usuario a la hora de escribir en un print
