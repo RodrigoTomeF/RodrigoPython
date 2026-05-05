4852af76f96f8104e65a7cf8a348bd6d43da65a4
RodrigoTomeF <rodritomeflo@gmail.com>
2026-05-05 13:18:19 -0400

    ensayo para prueba

--- randomly.py
+++ randomly.py
@@ -87,3 +87,110 @@ import random
 
 
 #ponerle un imite al usuario a la hora de escribir en un print
+
+import time 
+
+
+# lanzamiento1=random.randint(1,9)
+# lanzamiento2=random.randint(1,9)
+# lanzamiento3=random.randint(1,9)
+# numero_adivi1=3
+# numero_adivi2=7
+# numero_adivi3=2
+# while True:
+#     print(f"Primer lanzamiento sale {lanzamiento1} y el núnmero a adivinar es {numero_adivi1}")
+#     if lanzamiento1 == numero_adivi1:
+#         print("El primer número es correcto")
+#     else:
+#         print("El primer número no es correcto")
+
+#     print(f"Primer lanzamiento sale {lanzamiento2} y el núnmero a adivinar es {numero_adivi2}")
+#     if lanzamiento2 == numero_adivi2:
+#         print("El primer número es correcto")
+#     else:
+#         print("El primer número no es correcto")
+
+
+#     print(f"Primer lanzamiento sale {lanzamiento3} y el núnmero a adivinar es {numero_adivi3}")
+#     if lanzamiento1 == numero_adivi3:
+#         print("El primer número es correcto")
+#     else:
+#         print("El primer número no es correcto")
+
+#     time.sleep(2)
+
+
+# lanzamiento1=random.randint(1,9)
+# lanzamiento2=random.randint(1,9)
+# lanzamiento3=random.randint(1,9)
+# t1=False
+# t2=False
+# t3=False
+# nums=0
+# print(f"Los numeros generados son: {lanzamiento1}, {lanzamiento2}, {lanzamiento3}")
+# while not t1 or not t2 or not t3:
+#     numerito=random.randint(1,9)
+#     print("El numero es", numerito)
+#     time.sleep(1)
+#     if numerito==lanzamiento1(1,9):
+#         t1=True
+#     if numerito==lanzamiento2(1,9):
+#         t2=True
+#     if numerito==lanzamiento3(1,9):
+#         t3=True
+#     nums+=1
+# print(f"GANASTE, en {nums} turnos")
+
+
+'''
+Fabrica de enlatados
+Se necestita hacer el algoritmo de productos enlatados
+se debe consultar el peso del producto (en gramos) (solo valores positivos)
+El porcentaje de sodio en el (solo valores entre 1 y 100)
+y si se va a vender nacional o internacionalmente
+considerar los criterios en la siguiente tabla 
+
+menos de 500 grs, lata normal
+501 hasta 1500 grs, lata mediana
+1501 y más, lata grande.
+si el sodio es menos de 5%, lata queda igual 
+si e entre 5% y 8% lata especial
+si tienes 9% o más, lata acorazada
+a las latas internacionales, se le debe pegar 
+un sticker de validació sanitaria 
+
+el:800, 7%, lima==> lata mediana especial con sticker sanitario
+'''
+
+
+
+peso_del_producto=int(input("Ingrese el peso del producto: "))
+porcentaje_de_sodio=int(input("Ingrese el porcentaje de sodio: "))
+Lata_envio=int(input("Envio internacional ?: "))
+
+
+if peso_del_producto <= 500:
+    tipo_de_lata="lata pequeña"
+elif peso_del_producto <= 1500:
+    tipo_de_lata= "lata mediana"
+else:
+    tipo_de_lata= "lata grande"
+
+if porcentaje_de_sodio <= 5:
+    porcentaje=""
+elif porcentaje_de_sodio <= 8: 
+    porcentaje="especial"
+else: 
+    print("usted eligio la lata acorazada")
+    porcentaje="acorazado"
+
+if Lata_envio == "internacional":
+    envio="con stiker sanitario"
+
+print(f"Lata {tipo_de_lata} {porcentaje} {envio}")
+
+
+# while peso_del_producto<1:
+#     print("Ingrese solo valores positivos")
+#     peso_del_producto=int(input("Ingrese el porcentaje de sodio del producto: "))
+
