017d445960ef3e87ed337beb69417757112cd8d7
RodrigoTomeF <rodritomeflo@gmail.com>
2026-05-12 12:32:24 -0400

    truExcept

---
+++ truExcept.py
@@ -0,0 +1,42 @@
+
+#El try es para darle un aviso a una persona de poeque fallo
+
+# while True:
+#     try:
+#         num=int(input("Ingrese un número: "))
+#         break
+#     except ValueError as er:
+#         print("Error", er)
+#         print("Solo debe ingresar números enteros")
+#     else:
+#         print("")
+
+
+op=0
+total=0
+while op!=4:
+    try:
+        print("1.- PC $500.000")
+        print("2.- LGTV 55 pulgadas $450.000")
+        print("3.- Microondas Mademsa $100.000")
+        print("4.- Salir")
+        print("Seleccione una opción")
+        op=int(input())
+    except ValueError as e:
+        print("Error", e)
+        print("Ingrese solo las opciones indicadas")
+    match op:
+        case 1: 
+            print("El total a pagar es ",500000*1.19)
+            total+=500000*1.19
+        case 2:
+            print("El total a pagar es ",450000*1.19)
+            total+=450000*1.19
+        case 3:
+            print("El total a pagar es ",100000*1.19)
+            total+=100000*1.19
+        case 4:
+            print("Saliendo")
+            print("El total a pagar es", total)
+        case _:
+            print("Seleccion invalida")
