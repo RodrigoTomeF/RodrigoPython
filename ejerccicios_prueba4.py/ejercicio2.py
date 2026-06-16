a59b2ed5f30a6e3feb4f3aa843f34acf10629202
RodrigoTomeF <rodritomeflo@gmail.com>
2026-06-16 12:56:22 -0400

    ejerccicios_prueba4

---
+++ ejerccicios_prueba4.py/ejercicio2.py
@@ -0,0 +1,17 @@
+#crear un gestor de pacientes
+
+paceintes=[
+    {"nombre": "Aquiles Baeza", 
+    "prevision": "fonasa", "temperatura":34.6, "grave": False}
+]
+
+#Crear al geston de pacientes en un centro medico
+#para poner el nombre se debe validar que no este vacio
+#y ademas tenga mas de 8 caracteres
+#para la prevision de salud solo existen 3 posibles valores
+#fonas, isapre, o fodesa
+#Al ingresar un paciente, se debe poner la temperatura
+#crear una funcion que valide si esta grave o no
+#para que este grave debe tener mas de 39
+
+
