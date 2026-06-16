02a0ea029352a324faf329afeb2ecc8388734173
RodrigoTomeF <rodritomeflo@gmail.com>
2026-06-16 13:31:39 -0400

    ejerccicios_prueba4

--- ejerccicios_prueba4.py/ejercicio2.py
+++ ejerccicios_prueba4.py/ejercicio2.py
@@ -1,11 +1,7 @@
 #crear un gestor de pacientes
 
-paceintes=[
-    {"nombre": "Aquiles Baeza", 
-    "prevision": "fonasa", "temperatura":34.6, "grave": False}
-]
 
-#Crear al geston de pacientes en un centro medico
+#Crear al gestor de pacientes en un centro medico
 #para poner el nombre se debe validar que no este vacio
 #y ademas tenga mas de 8 caracteres
 #para la prevision de salud solo existen 3 posibles valores
@@ -13,5 +9,30 @@ paceintes=[
 #Al ingresar un paciente, se debe poner la temperatura
 #crear una funcion que valide si esta grave o no
 #para que este grave debe tener mas de 39
+
+
+pacientes=[
+    {"nombre": "Aquiles Baeza", 
+    "prevision": "fonasa", "temperatura":34.6, "grave": False}
+]
+
+def mostrar_prevision():
+    print("fonasa")
+    print("isapre")
+    print("fodesa")
+
+temperatura_del_paciente=0
+ 
+print("-"*40)
+print("gestor de pacientes")
+nombre_del_paciente=input("Ingrese nombre del paciente: ")
+mostrar_prevision()
+prevision_del_paceinte=input("Ingrese la prevision de su uso: ")
+temperatura_del_paciente=int(input("Ingrese la temperatura del paciente"))
+if temperatura_del_paciente >= 39:
+    grave = True
+else:
+    grave = False
+pacientes[list(pacientes)[-1]+1]={"nombre": nombre_del_paciente, "prevision":prevision_del_paceinte, "temperatura":temperatura_del_paciente}
 
 
